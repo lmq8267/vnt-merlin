8219d41d789921f7cc782fafe742ddd6d634b052
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2024-02-22 22:13:43 +0800

    Create install.sh

---
+++ koolshare/vnt/install.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+
+source /koolshare/scripts/base.sh
+eval `dbus export vnt_`
+alias echo_date='echo 【$(TZ=UTC-8 date -R +%Y年%m月%d日\ %X)】:'
+
+DIR=$(cd $(dirname $0); pwd)
+
+en=`dbus get vnt_enable`
+en2=`dbus get vnts_enable`
+
+if [ ! -d "/koolshare" ] ; then
+  echo_date "你的固件不是koolshare梅林，无法安装此插件包，请正确选择插件包！"
+  rm -rf /tmp/vnt* >/dev/null 2>&1
+  exit 1
+fi
+if [ "${en}"x = "1"x ] || [ "${en2}"x = "1"x ] ; then
+    sh /koolshare/scripts/vnt_config.sh stop
+fi
+find /koolshare/init.d/ -name "*vnt.sh*"|xargs rm -rf
+cd /tmp
+
+cp -rf /tmp/vnt/scripts/* /koolshare/scripts/
+cp -rf /tmp/vnt/webs/* /koolshare/webs/
+cp -rf /tmp/vnt/res/* /koolshare/res/
+cp /tmp/vnt/uninstall.sh /koolshare/scripts/uninstall_vnt.sh
+
+
+
+chmod +x /koolshare/scripts/vnt_*
+dbus set softcenter_module_vnt_description=简便高效的异地组网、内网穿透工具
+dbus set softcenter_module_vnt_install=1
+dbus set softcenter_module_vnt_name=vnt
+dbus set softcenter_module_vnt_title=vnt
+dbus set softcenter_module_vnt_version="$(cat $DIR/version)"
+
+sleep 1
+echo_date "vnt 插件安装完毕！"
+rm -rf /tmp/vnt* >/dev/null 2>&1
+en=`dbus get vnt_enable`
+en2=`dbus get vnts_enable`
+if [ "${en}"x = "1"x ] || [ "${en2}"x = "1"x ] ; then
+    sh /koolshare/scripts/vnt_config.sh restart
+fi
+exit 0
