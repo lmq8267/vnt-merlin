03664017d31a09cdd86fc4dd06b991007370e533
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2024-02-27 12:08:11 +0800

    Update vnt_config.sh

--- koolshare/vnt/scripts/vnt_config.sh
+++ koolshare/vnt/scripts/vnt_config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-eval `dbus export vnt_`
 source /koolshare/scripts/base.sh
+eval `dbus export vnt_`
 mkdir -p /tmp/upload
 mkdir -p /home/root/log
 touch /home/root/log/vnt-cli.log
@@ -98,7 +98,7 @@ logg () {
 # 自启
 fun_nat_start(){
     if [ "${vnt_enable}"x = "1"x ] || [ "${vnts_enable}"x = "1"x ];then
-	    [ ! -L "/koolshare/init.d/N99vnt.sh" ] && ln -sf /koolshare/scripts/vnt_config.sh /koolshare/init.d/N99vnt.sh
+	    [ ! -L "/koolshare/init.d/S99vnt.sh" ] && ln -sf /koolshare/scripts/vnt_config.sh /koolshare/init.d/S99vnt.sh
     fi
 }
 # 定时任务
@@ -645,6 +645,15 @@ fi
 	fun_start_vnts
 	http_response "$1"
     ;;
+*)
+if [ "${vnt_enable}" != "1" ] && [ "${vnts_enable}" != "1" ] ; then
+logger "【软件中心】 : 未开启vnt 无需启动..."
+   exit  
+fi
+	fun_start_stop
+	fun_nat_start
+	fun_crontab
+	;;
 esac
 # 界面提交的参数
 case $2 in
